eaacdde786b95426da27d659edb5289864715fe9
Luis Parra <luis.parramorales@arup.com>
2025-06-22 16:12:04 +0100

    Format CLI with ruff

--- src/metro_disruptions_intelligence/cli.py
+++ src/metro_disruptions_intelligence/cli.py
@@ -7,6 +7,8 @@ import click
 @click.command()
 def cli(args=None):
     """Console script for metro_disruptions_intelligence."""
-    click.echo("Replace this message by putting your code into metro_disruptions_intelligence.cli.cli")
+    click.echo(
+        "Replace this message by putting your code into metro_disruptions_intelligence.cli.cli"
+    )
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
